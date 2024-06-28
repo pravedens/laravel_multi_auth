5b442854d52c711b12c473d0612637b0e77685ce
pravedens <107057525+pravedens@users.noreply.github.com>
2024-06-28 19:31:36 +0500

    Аутентификация пользователя

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectTo(
+            guests: '/account/login',
+            users: '/account/dashboard'
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/account/login', [LoginController::class, 'index'])->name('account.login');
-Route::get('/account/logout', [LoginController::class, 'logout'])->name('account.logout');
-Route::get('/account/register', [LoginController::class, 'register'])->name('account.register');
-Route::post('/account/process-register', [LoginController::class, 'processRegister'])->name('account.processRegister');
+Route::group(['prefix' => 'account'], function() {
+    Route::group(['middleware' => 'guest'], function() {
+        Route::get('login', [LoginController::class, 'index'])->name('account.login');
+        Route::get('register', [LoginController::class, 'register'])->name('account.register');
+        Route::post('process-register', [LoginController::class, 'processRegister'])->name('account.processRegister');
+        Route::post('authenticate', [LoginController::class, 'authenticate'])->name('account.authenticate');
+    });
+    Route::group(['middleware' => 'auth'], function() {
+        Route::get('logout', [LoginController::class, 'logout'])->name('account.logout');
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('account.dashboard');
+    });
+});
 
-Route::post('/account/authenticate', [LoginController::class, 'authenticate'])->name('account.authenticate');
-Route::get('/account/dashboard', [DashboardController::class, 'index'])->name('account.dashboard');
